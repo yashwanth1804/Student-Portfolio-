14b8e95e2d95a2c46a775aa7e4218278ac328993
yashwanth_s <88342038+yashwanth1804@users.noreply.github.com>
2024-07-25 18:31:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    setInterval(loadIntro, 2000)
+  }
+  
+  var intros = ["front end developer","Student"]
+  var index = 0;
+  var intro = document.getElementById("intro-loader")
+  
+  function loadIntro() {
+    intro.innerHTML = intros[index];
+    intro.classList.add("fade");
+    index++;
+    if (index >= intros.length) {
+      index = 0;
+    }
+  }
+  
+  function messageCount() {
+    var msg = document.getElementById("message").value;
+    var msgCount = document.getElementById("message-count")
+    var msgLength = msg.length;
+    const maxLength = 1000;
+    var charLeft = maxLength - msgLength;
+    msgCount.innerText = charLeft;
+  }
